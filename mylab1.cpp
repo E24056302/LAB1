539bb3cbdf5e83fa794bd2c566b0c15f77222a24
E24056302 <vx14@kimo.com>
2017-03-21 00:05:49 +0800

    first commit

---
+++ mylab1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+	int  n;
+	cout<<"please enter a number!!"<<endl;
+	cin>>n;
+if(n==1){
+cout<<n<<endl;
+}
+	while(n>1){
+
+if(n%2==1){
+n=3*n+1;
+}
+else{
+n=n/2;
+}
+cout<<n<<endl;
+}
+
+return 0;
+}
+
